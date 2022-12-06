92e5b25c640cf78b752e39798827796528cd6529
naeem <naeemqg1@gmail.com>
2022-12-06 18:56:03 +0530

    add components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import "./style.css";
+import "./style/style.css";
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <React.StrictMode>
